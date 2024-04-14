5a897dc8b1aa59aa1c3d4190c41f0c405ae78d78
Whatbar123 <162871104+Whatbar123@users.noreply.github.com>
2024-04-14 11:02:14 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var div = document.getElementById("myDiv");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  });
