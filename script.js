c69294f3e5c3dc5645f723fcaa72c95671736b88
Whatbar123 <162871104+Whatbar123@users.noreply.github.com>
2024-04-21 08:14:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    var div = document.getElementById("myDiv");
-    if (div.style.display === "none") {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
-  });
+document.getElementById('showSophia').addEventListener('click', function() {
+  document.getElementById('sophiaInfo').style.display = 'block';
+});
